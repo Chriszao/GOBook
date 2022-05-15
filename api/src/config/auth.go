915d14d4f199d68095280c42f7892d6ed2a6629c
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 18:54:19 -0300

    feat(backend): Add GenerateToken function to config package

---
+++ api/src/config/auth.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func GenerateToken(userId uint64) (string, error) {
+	claims := jwt.MapClaims{}
+
+	claims["authorized"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	claims["userId"] = userId
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(JwtSecretKey)
+}
