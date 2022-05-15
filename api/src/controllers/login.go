830598f3bf3551e2452faafe1a42e944f4ca0b18
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 18:54:50 -0300

    feat(backend): Add Login controller

---
+++ api/src/controllers/login.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"api/src/config"
+	"api/src/database"
+	"api/src/models"
+	"api/src/providers"
+	"api/src/repositories"
+	"api/src/responses"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func Login(writer http.ResponseWriter, request *http.Request) {
+	requestBody, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		responses.Error(writer, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var user models.User
+
+	if err := json.Unmarshal(requestBody, &user); err != nil {
+		responses.Error(writer, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		responses.Error(writer, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+
+	userExists, err := repository.FindByEmail(user.Email)
+
+	if err != nil {
+		responses.Error(writer, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := providers.ValidatePassword(user.Password, userExists.Password); err != nil {
+		responses.Error(writer, http.StatusUnauthorized, err)
+		return
+	}
+
+	token, _ := config.GenerateToken(userExists.ID)
+	fmt.Println(token)
+}
