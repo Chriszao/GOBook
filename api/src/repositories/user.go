2cf657fba86e914e67ed0ad2cadd991166029c05
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 16:46:12 -0300

    feat(backend): Add Update method user repository function

--- api/src/repositories/user.go
+++ api/src/repositories/user.go
@@ -101,3 +101,26 @@ func (repository User) FindById(id uint64) (models.User, error) {
 
 	return user, nil
 }
+
+func (repository User) Update(id uint64, user models.User) error {
+	statement, err := repository.db.Prepare(
+		"UPDATE user SET name = ?, nick = ?, email = ? WHERE id = ?",
+	)
+
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	if _, err = statement.Exec(
+		user.Name,
+		user.Nick,
+		user.Email,
+		id,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
