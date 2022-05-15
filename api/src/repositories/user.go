937c3758a25c51379e1800f0ab9be30dc399d715
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 15:21:54 -0300

    feat(backend): Add create funciton to user repository

--- api/src/repositories/user.go
+++ api/src/repositories/user.go
@@ -13,6 +13,28 @@ func NewUserRepository(db *sql.DB) *User {
 	return &User{db}
 }
 
-func (userRepository User) Create(user models.User) (uint64, error) {
-	return 0, nil
+func (repository User) Create(user models.User) (uint64, error) {
+	statement, err := repository.db.Prepare(
+		"INSERT INTO user (name, nick, email, password) VALUES (?, ?, ?, ?)",
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(user.Name, user.Nick, user.Email, user.Password)
+
+	if err != nil {
+		return 0, err
+	}
+
+	userId, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return uint64(userId), nil
 }
