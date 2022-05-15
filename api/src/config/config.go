e5c180d7f5613068490c9557f2365e861bb497fa
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 14:35:47 -0300

    feat(backend): Add config package to load environment variables

---
+++ api/src/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// MySQL connection string
+	ConnectionString = ""
+
+	// API port
+	Port = 0
+)
+
+// Load evironment variables
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+
+	if err != nil {
+		Port = 9000
+	}
+
+	ConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
