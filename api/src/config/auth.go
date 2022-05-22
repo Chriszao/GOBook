0c8d23ef94ad6dd637570c4e930e094ba1a7c497
Chriszao <chris.f.assis18@gmail.com>
2022-05-22 19:11:25 -0300

    feat(backend): Add functions to validate token

--- api/src/config/auth.go
+++ api/src/config/auth.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -16,4 +20,39 @@ func GenerateToken(userId uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(JwtSecretKey)
+}
+
+// Verifies if the token is valid
+func ValidateToken(request *http.Request) error {
+	tokenString := getToken(request)
+
+	token, err := jwt.Parse(tokenString, keyFunc)
+
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("invalid token")
+}
+
+func getToken(request *http.Request) string {
+	token := request.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signature method %v", token.Header["alg"])
+	}
+
+	return JwtSecretKey, nil
 }
